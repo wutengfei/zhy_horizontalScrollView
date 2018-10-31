9ee602ca991dd57e1358a5525e2475714bd7bda5
wutengfei <781862614@qq.com>
2018-10-31 17:47:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhy_horizontalscrollview/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.zhy_horizontalscrollview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+import com.example.zhy_horizontalscrollview.MyHorizontalScrollView.CurrentImageChangeListener;
+import com.example.zhy_horizontalscrollview.MyHorizontalScrollView.OnItemClickListener;
+
+public class MainActivity extends Activity {
+
+    private MyHorizontalScrollView mHorizontalScrollView;
+    private HorizontalScrollViewAdapter mAdapter;
+    private ImageView mImg;
+    private List<Integer> mDatas = new ArrayList<Integer>(Arrays.asList(
+            R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4,
+            R.drawable.a5, R.drawable.a6, R.drawable.a7, R.drawable.a8,
+            R.drawable.a9));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        mImg = (ImageView) findViewById(R.id.id_content);
+
+        mHorizontalScrollView = (MyHorizontalScrollView) findViewById(R.id.id_horizontalScrollView);
+        mAdapter = new HorizontalScrollViewAdapter(this, mDatas);
+        //添加滚动回调
+        mHorizontalScrollView
+                .setCurrentImageChangeListener(new CurrentImageChangeListener() {
+                    @Override
+                    public void onCurrentImgChanged(int position,
+                                                    View viewIndicator) {
+                        mImg.setImageResource(mDatas.get(position));
+                        viewIndicator.setBackgroundColor(Color
+                                .parseColor("#AA024DA4"));
+                    }
+                });
+        //添加点击回调
+        mHorizontalScrollView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onClick(View view, int position) {
+                mImg.setImageResource(mDatas.get(position));
+                view.setBackgroundColor(Color.parseColor("#AA024DA4"));
+            }
+        });
+        //设置适配器
+        mHorizontalScrollView.initDatas(mAdapter);
+    }
+
+}
